e3551a315049d49c0c7a2685de0b85ddb9c3472d
Coriscool <paulwolting@gmail.com>
2023-10-23 12:25:24 +0200

    Hij checkmate nu ook
    *feesttoeter geluid*

--- js/Board.js
+++ js/Board.js
@@ -40,8 +40,8 @@ export default class Board {
         tiles[1][7] = new Knight(1, 7, COLOUR.WHITE, '♘', 3);
         tiles[6][7] = new Knight(6, 7, COLOUR.WHITE, '♘', 3);
 
-        tiles[4][0] = new King(4, 0, COLOUR.BLACK, '♚', -900);
-        tiles[4][7] = new King(4, 7, COLOUR.WHITE, '♔', 900);
+        tiles[4][0] = new King(4, 0, COLOUR.BLACK, '♚', -200);
+        tiles[4][7] = new King(4, 7, COLOUR.WHITE, '♔', 200);
 
         tiles[3][0] = new Queen(3, 0, COLOUR.BLACK, '♛', -10);
         tiles[3][7] = new Queen(3, 7, COLOUR.WHITE, '♕', 10);
@@ -127,13 +127,27 @@ export default class Board {
         let calculating = false;
         const x = Math.floor(clientX / 100);
         const y = Math.floor(clientY / 100);
+        const Aicolour = COLOUR.BLACK;
+        const PlayerColour = COLOUR.WHITE;
         if(!calculating){
-            this.select(x,y);
+            if (this.isInCheck) {
+                let moves = CheckFinder.findMovesForCheckedPlayer(this.tiles, this.turn);
+                if (moves.length === 0) {
+                    console.log('Checkmate');
+                    fill(10,10,10);
+                    textFont('Arial');
+                    text('Checkmate',400,400,500,500);
+                    noLoop();
+                }
+            }
+            else{
+                this.select(x,y);
+            }
             //console.clear();
         }
-        if(this.turn === COLOUR.BLACK){
+        if(this.turn === Aicolour){
             calculating = true;
-            let allMoves1 = this.findAllMoves(COLOUR.BLACK, this.tiles);
+            let allMoves1 = this.findAllMoves(Aicolour, this.tiles);
             if (allMoves1.length === 0 && !this.isInCheck) {
                 console.log("Draw by stalemate");
                 fill(10, 10, 10);
@@ -143,10 +157,10 @@ export default class Board {
             }
             if (allMoves1.length !== 0) {
                 const boardstate = _.cloneDeep(this.tiles);
-                let bestMove = this.chessLooper(allMoves1, 3, COLOUR.BLACK, boardstate);
+                let bestMove = this.chessLooper(allMoves1, 3, Aicolour, boardstate);
                 this.move(this.tiles[bestMove.from.i][bestMove.from.j], bestMove.to, this.tiles);
             }
-            this.turn = COLOUR.WHITE;
+            this.turn = PlayerColour;
             calculating = false;
         }
     }
@@ -223,19 +237,6 @@ export default class Board {
         return x > 7 || x < 0 || y > 7 || y < 0;
     }
 
-
-    evaluator() {
-        let evaluation = 0;
-        for(let i = 0; i<8; i++){
-            for(let j = 0; j<8; j++){
-                if(this.tiles[i][j] != undefined){
-                    evaluation += this.tiles[i][j].value;
-                }
-            }
-        }
-        return evaluation;
-    }
-
     findAllMoves (colour, tiles) {
         let possibleMovable = [];
         let possibleMove = [];
@@ -268,20 +269,36 @@ export default class Board {
         return possibleMove;
     }
 
+    evaluator() {
+        let evaluation = 0;
+        for(let i = 0; i<8; i++){
+            for(let j = 0; j<8; j++){
+                if(this.tiles[i][j] != undefined){
+                    evaluation += this.tiles[i][j].value;
+                }
+            }
+        }
+        return evaluation;
+    }
+
     resetBoard(BoardNeedsToBecome) {
         this.tiles = _.cloneDeep(BoardNeedsToBecome);
+    }
+
+    shouldITrade (color, moveValue, maxMoveValue) {
+        if (color === COLOUR.BLACK) {
+            return moveValue <= maxMoveValue;
+        }
+        return moveValue >= maxMoveValue;
     }
 
     chessLooper (allMoves1, depth, color, boardstate) {
         if (allMoves1.length === 0) {
             this.isInCheck = CheckFinder.isCurrentPlayerInCheck(boardstate, this.turn);
-            console.log(this.isInCheck);
             if (this.isInCheck) {
-                console.log('could be checkmate');
                 return 'checkmate';
             }
             if (!this.isInCheck) {
-                console.log('could be draw');
                 return 'draw';
             }
         }
@@ -292,37 +309,37 @@ export default class Board {
 
         if (colour == COLOUR.BLACK) {
             colour = COLOUR.WHITE;
-            maxMoveValue = 1000;
+            maxMoveValue = 500;
         }
         else {
             colour = COLOUR.BLACK;
-            maxMoveValue = -1000;
+            maxMoveValue = -500;
         }
 
-        depth = depth - 1;
+        depth --;
 
         for (let j = 0; j < allMoves1.length; j++) {
             this.move(this.tiles[allMoves1[j].from.i][allMoves1[j].from.j], allMoves1[j].to, this.tiles);
-            //console.log(boardstate);
-            //this.move(boardstate[allMoves1[j].from.i][allMoves1[j].from.j], allMoves1[j].to, boardstate);
 
             if (depth > 0) {
                 let copyOfTiles = _.cloneDeep(this.tiles);
                 bestMove = this.chessLooper(this.findAllMoves(colour, this.tiles), depth, colour, copyOfTiles);
                 if  (bestMove == 'draw')    {
-                    console.log(bestMove, 'draw');
                     allMoves1[j].valueOfMove = 0;
                 }
                 if  (bestMove == 'checkmate')   {
-                    console.log(bestMove, 'checkmate');
-                    allMoves1[j].valueOfMove = -1*maxMoveValue;
+                    if (colour == COLOUR.BLACK) {
+                        allMoves1[j].valueOfMove = 500;
+                    }
+                    else {
+                        allMoves1[j].valueOfMove = -500;
+                    }
                 }
                 else    {
                     allMoves1[j].valueOfMove = bestMove.valueOfMove;
                 }
             }
             else {
-                console.log('calculating...');
                 allMoves1[j].valueOfMove = this.evaluator();
             }
 
@@ -334,20 +351,46 @@ export default class Board {
         }
         return allMoves1[bestMoveIndex];
     }
-    // resetBoard(boardstate){
-    //     this.tiles[boardstate.from.x][boardstate.from.y] = JSON.parse(JSON.stringify(boardstate.from));
+
+
+
+    //Dit hieronder is mijn poging om minder _.deepclone() the gebruiken...
+
+    // evaluator(boardToEvaluate) {
+    //     let evaluation = 0;
+    //     for(let i = 0; i<8; i++){
+    //         for(let j = 0; j<8; j++){
+    //             if(boardToEvaluate[i][j] != undefined){
+    //                 evaluation += boardToEvaluate[i][j].value;
+    //             }
+    //         }
+    //     }
+    //     console.log(boardToEvaluate);
+    //     return evaluation;
+    // }
+
+    // resetBoard(boardstate, boardToReset){
+    //     boardToReset[boardstate.from.x][boardstate.from.y] = _.cloneDeep(boardstate.from);
     //     if (boardstate.to === undefined) {
-    //         this.tiles[boardstate.toX][boardstate.toY] = undefined;
+    //         boardToReset[boardstate.toX][boardstate.toY] = undefined;
     //     }
     //     else {
-    //         this.tiles[boardstate.toX][boardstate.toY] = JSON.parse(JSON.stringify(boardstate.to));
+    //         boardToReset[boardstate.toX][boardstate.toY] = _.cloneDeep(boardstate.to);
     //     }
     // }
 
-    // chessLooper (allMoves1, depth, color) {
+    // chessLooper (allMoves1, depth, color, boardImput) {
     //     if (allMoves1.length === 0) {
-    //         console.log('draw');
-    //         return 'draw';
+    //         this.isInCheck = CheckFinder.isCurrentPlayerInCheck(boardImput, this.turn);
+    //         console.log(this.isInCheck, 'hi');
+    //         if (this.isInCheck) {
+    //             console.log('could be checkmate');
+    //             return 'checkmate';
+    //         }
+    //         if (!this.isInCheck) {
+    //             console.log('could be draw');
+    //             return 'draw';
+    //         }
     //     }
         
     //     let colour = color;
@@ -355,6 +398,7 @@ export default class Board {
     //     let bestMoveIndex = -1;
     //     let bestMove = undefined;
     //     let maxMoveValue = undefined;
+
     //     if (colour == COLOUR.BLACK) {
     //         colour = COLOUR.WHITE;
     //         maxMoveValue = 1000;
@@ -368,53 +412,45 @@ export default class Board {
 
     //     for (let j = 0; j < allMoves1.length; j++) {
     //         let boardstate = undefined;
-    //         if(this.tiles[allMoves1[j].to.x][allMoves1[j].to.y] == undefined){
-    //             boardstate = {from: JSON.parse(JSON.stringify(this.tiles[allMoves1[j].from.i][allMoves1[j].from.j])), to: undefined, toX: allMoves1[j].to.x, toY: allMoves1[j].to.y};
+    //         if(boardImput[allMoves1[j].to.x][allMoves1[j].to.y] == undefined){
+    //             //boardstate = {from: JSON.parse(JSON.stringify(this.tiles[allMoves1[j].from.i][allMoves1[j].from.j])), to: undefined, toX: allMoves1[j].to.x, toY: allMoves1[j].to.y};
+    //             let pieceIMovedFrom = _.cloneDeep(boardImput[allMoves1[j].from.i][allMoves1[j].from.j]);
+    //             boardstate = {from: pieceIMovedFrom, to: undefined, toX: allMoves1[j].to.x, toY: allMoves1[j].to.y};
     //         }
     //         else{
-    //             boardstate = {from: JSON.parse(JSON.stringify(this.tiles[allMoves1[j].from.i][allMoves1[j].from.j])), to: JSON.parse(JSON.stringify(this.tiles[allMoves1[j].to.x][allMoves1[j].to.y])), toX: JSON.parse(JSON.stringify(allMoves1[j].to.x)), toY: JSON.parse(JSON.stringify(allMoves1[j].to.y))};
+    //             //boardstate = {from: JSON.parse(JSON.stringify(this.tiles[allMoves1[j].from.i][allMoves1[j].from.j])), to: JSON.parse(JSON.stringify(this.tiles[allMoves1[j].to.x][allMoves1[j].to.y])), toX: JSON.parse(JSON.stringify(allMoves1[j].to.x)), toY: JSON.parse(JSON.stringify(allMoves1[j].to.y))};
+    //             let pieceIMovedFrom = _.cloneDeep(boardImput[allMoves1[j].from.i][allMoves1[j].from.j]);
+    //             boardstate = {from: pieceIMovedFrom, to: boardImput[allMoves1[j].to.x][allMoves1[j].to.y], toX: allMoves1[j].to.x, toY: allMoves1[j].to.y}
     //         }
-    //         console.log (boardstate);
-    //         this.move(this.tiles[allMoves1[j].from.i][allMoves1[j].from.j], allMoves1[j].to);
+
+    //         this.move(boardImput[allMoves1[j].from.i][allMoves1[j].from.j], allMoves1[j].to, boardImput);
             
     //         if (depth > 0) {
-    //             bestMove = this.chessLooper(this.findAllMoves(colour), depth, colour);
-    //             allMoves1[j].valueOfMove = bestMove.valueOfMove;
+    //             bestMove = this.chessLooper(this.findAllMoves(colour, boardImput), depth, colour, boardImput);
+    //             if  (bestMove == 'draw')    {
+    //                  console.log(bestMove, 'draw');
+    //                  allMoves1[j].valueOfMove = 0;
+    //              }
+    //              if  (bestMove == 'checkmate')   {
+    //                  console.log(bestMove, 'checkmate');
+    //                  allMoves1[j].valueOfMove = -1*maxMoveValue;
+    //              }
+    //              else    {
+    //                  allMoves1[j].valueOfMove = bestMove.valueOfMove;
+    //              }
     //         }
     //         else {
-    //             allMoves1[j].valueOfMove = this.evaluator();
+    //             console.log('calculating...');
+    //             allMoves1[j].valueOfMove = this.evaluator(boardImput);
     //         }
 
     //         if (this.shouldITrade(color, allMoves1[j].valueOfMove, maxMoveValue)) {
     //             maxMoveValue = allMoves1[j].valueOfMove;
     //             bestMoveIndex = j;
     //         }
-    //         this.resetBoard(boardstate);
+    //         this.resetBoard(boardstate, boardImput);
     //     }
     //     return allMoves1[bestMoveIndex];
     // }
-
-    
-
-    // shouldITrade (color, moveValue, maxMoveValue) {
-    //     let tradeGood = false;
-    //     if (color === COLOUR.BLACK) {
-    //         if (moveValue <= maxMoveValue) {
-    //             tradeGood = true;
-    //         }
-    //     }
-    //     if (color === COLOUR.WHITE) {
-    //         if (moveValue >= maxMoveValue) {
-    //             tradeGood = true;
-    //         }
-    //     }
-    //     return tradeGood;
-    // }
-    shouldITrade (color, moveValue, maxMoveValue) {
-        if (color === COLOUR.BLACK) {
-            return moveValue <= maxMoveValue;
-        }
-        return moveValue >= maxMoveValue;
-    }
 }
 //♟♙♜♖♝♗♞♘♚♔♛♕
